a00db02f47e7ede078b5ca05e18d7c57c526f7d1
ekip <sammyetur11@gmail.com>
2023-10-21 02:51:51 +0000

    add gitsigns

--- plugins/user.lua
+++ plugins/user.lua
@@ -13,4 +13,9 @@ return {
     dependencies = { "romgrk/fzy-lua-native" },
     config = function() require "user.wilder" end,
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "CursorHold", "CursorHoldI" },
+    config = function() require "user.gitsigns" end,
+  },
 }
